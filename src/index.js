efafbcdd2999c0719f70f2edf47b8f885b9fcbc9
vertraut <onlinemovement@gmail.com>
2020-10-26 02:20:28 +0200

    рендер разметки

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import {themeSwithcer, setTheme, themeSwitcherEl } from './js/theme-switcher'
+import { themeSwithcer, setTheme, themeSwitcherEl } from './js/theme-switcher'
+import cardsMenuMarkup from './js/card-menu-markup'
+
 
 setTheme(); //устанавливаем тему из localStorage или по-умолчанию
 
 themeSwitcherEl.addEventListener('change', themeSwithcer)//отслеживание переключения темы
 
-
+cardsMenuMarkup();
 
 
 
